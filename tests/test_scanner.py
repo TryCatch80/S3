8c62d120f3dc8481c78c6d2c3e66400e2adc6e36
Shai rod <srod@salesforce.com>
2022-12-27 14:03:56 +0200

    Initial commit

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -95,8 +95,10 @@ def test_check_aws_creds():
     test_setup()
     # Check more thoroughly for creds being set.
     vars = os.environ
-    keyid = "ASIAVQT2Q7OAH543FG6J"
-    key = "ASIAVQT2Q7OAH543JG6U"
+    AWS_ACCESS_KEY_ID = 'ASIAVQT2Q7OAH543JG6U'
+    AWS_SECRET_ACCESS_KEY = 'ASIAVQT2Q7OAH543JG6U'
+    keyid = vars.get("AWS_ACCESS_KEY_ID")
+    key = vars.get("AWS_SECRET_ACCESS_KEY")
     credsFile = os.path.expanduser("~") + "/.aws/credentials"
     if keyid is not None and len(keyid) == 20:
         if key is not None and len(key) == 40:
